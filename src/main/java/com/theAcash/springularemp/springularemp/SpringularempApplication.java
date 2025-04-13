79d0fd851b9520e8926eb7ff4053baf1ad1a9c64
TheACash <arpitashwatch@gmail.com>
2025-04-13 10:24:10 +0530

    initialized

---
+++ src/main/java/com/theAcash/springularemp/springularemp/SpringularempApplication.java
@@ -0,0 +1,13 @@
+package com.theAcash.springularemp.springularemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringularempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringularempApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theAcash/springularemp/springularemp/SpringularempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theAcash.springularemp.springularemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringularempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
